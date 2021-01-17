670892a283dff5b064644fc90df1f469a206d29d
marti <marticraft680@gmail.com>
2021-01-17 01:22:12 -0500

    error handling

--- main.py
+++ main.py
@@ -12,15 +12,48 @@ import json
 import os
 
 
-def log(*args, end="\n"):
-    print(f"@[{datetime.datetime.now().strftime('%H:%M:%S')}] ", args, end=end)
+errorNote_newLine = "\n                   "
+
+
+def log(msg, end="\n"):
+    print(f"@[{datetime.datetime.now().strftime('%H:%M:%S')}]", msg, end=end)
+
+
+def error_note(*notes, sep="\n", end="\n"):
+    if sep[0] == '\n':
+        sep = errorNote_newLine + sep[1:]
+
+    print("  ^^^^^^^^   Note:", end=" ")
+    for note in notes:
+        for n in note:
+            if n is not note[-1]:
+                print(n, end=sep)
+            else:
+                print(n)
+    if '\n' not in sep:
+        print(end=end)
+
+
+def warn(warning: str, *msgs):
+    log(f"-Error-: {warning}")
+    error_note(msgs)
+
+
+def exit_error(error: str, msg):
+    exit(f"@[{datetime.datetime.now().strftime('%H:%M:%S')}] Error: {error}\n  ^^^^^^^^   Note: {msg}")
 
 
 class Student:
-    def __init__(self, name, id, password, schedule, counselor, email=''):
+    def __init__(self, name, id, password, schedule, counselor, email):
         self.name = name
         self.first_name = self.name.split(' ', 1)[0]
-        self.last_name = self.name.split(' ', 1)[1]
+        try:
+            self.last_name = self.name.split(' ', 1)[1]
+        except Exception:
+            exit_error(
+                "No Last Name Provided",
+                "Make sure you put your full name in \"student.json\""
+            )
 
         self.id = id
         self.password = password
@@ -39,14 +72,15 @@ class Student:
 
 
 class web_bot:
-    """If Anything fails, increase the sleep time"""
-
     def __init__(self):
-        chrome_options = Options()
-        chrome_options.add_argument("--use-fake-ui-for-media-stream")
-        self.browser = webdriver.Chrome(chrome_options=chrome_options)
-        # self.browser = webdriver.Chrome("chromedriver.exe")
-
+        """
+        Required properties:
+            student["name"],
+            student["id"],
+            student["password"],
+            student["schedule"],
+            student["counselor"]
+        """
         with open("student.json", 'r') as f:
             student = json.load(f)
             self.student = Student(
@@ -54,11 +88,28 @@ class web_bot:
                 student["id"],
                 student["password"],
                 student["schedule"],
-                student["counselor"]
+                student["counselor"],
+                student["email"]
             )
 
+        chrome_options = Options()
+        chrome_options.add_argument("--use-fake-ui-for-media-stream")
+        self.browser = webdriver.Chrome(chrome_options=chrome_options)
+        # self.browser = webdriver.Chrome("chromedriver.exe")
+
+    """If Anything fails, increase the wait time"""
+
     def google_logIn(self):
+        """
+        Required Properties:
+            student["email"],
+            student["password"]
+        May require:
+            student["name"]
+            student["id"]
+        """
         self.browser.get("https://accounts.google.com/signin/v2/identifier?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAAQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
+        log("Signing in to Google Account...")
 
         try:
             WebDriverWait(self.browser, 5).until(
@@ -71,8 +122,13 @@ class web_bot:
                 .find_element_by_class_name("VfPpkd-dgl2Hf-ppHlrf-sM5MNb")\
                 .find_element_by_class_name("VfPpkd-LgbsSe")\
                 .send_keys(Keys.RETURN)
-        except:
+        except Exception as error:
             self.browser.quit()
+            exit_error(
+                error,
+                "Could not load Google Sign In page{}".format(errorNote_newLine) +
+                "Re-run the program."
+            )
 
         try:
             WebDriverWait(self.browser, 5).until(
@@ -85,29 +141,62 @@ class web_bot:
             self.browser.find_element_by_id("passwordNext")\
                         .find_element_by_class_name("AjY5Oe")\
                         .send_keys(Keys.RETURN)
-        except:
+        except Exception as error:
             self.browser.quit()
-
-        log(f"Successfully logged in to Google account <{self.student.email}>")
-        time.sleep(1)
-
-    def submit_school_attendance(self):
-        self.browser.get("https://docs.google.com/forms/d/e/1FAIpQLSeob9jBxe46TJk8QR9qpzdHz_DBBY_AL7wzlLxCbDw7CDO9BA/viewform?gxids=7628")
+            exit_error(
+                error,
+                f"Email Address <{self.student.email}> does not exist{errorNote_newLine}"
+                "Make sure you have the right email address in \"student.json\""
+            )
 
         try:
-            WebDriverWait(self.browser, 5).until(
-                EC.presence_of_all_elements_located(
+            WebDriverWait(self.browser, 2).until(
+                EC.presence_of_element_located(
+                    (By.ID, "gsr")
+                )
+            )
+        except Exception as error:
+            self.browser.quit()
+            exit_error(
+                error,
+                "Your password is incorrect{}".format(errorNote_newLine) +
+                "Make sure your password is correct in \"student.json\""
+            )
+
+        log(f"Logged in to Google account <{self.student.email}>")
+
+    def submit_school_attendance(self):
+        """
+        Required properties:
+            Google - Logged in,
+            student["name"],
+            student["id"],
+            student["counselor"]
+        """
+        self.browser.get("https://docs.google.com/forms/d/e/1FAIpQLSeob9jBxe46TJk8QR9qpzdHz_DBBY_AL7wzlLxCbDw7CDO9BA/viewform?gxids=7628")
+        log("Completing School Attendance...")
+
+        try:
+            WebDriverWait(self.browser, 10).until(
+                EC.element_to_be_clickable(
                     (By.CLASS_NAME, "quantumWizTextinputPaperinputInput")
                 )
-            )[0].send_keys(bot.student.first_name)
+            )
+            self.browser.find_elements_by_class_name("quantumWizTextinputPaperinputInput")[0]\
+                        .send_keys(bot.student.first_name)
 
-            self.browser.find_elements_by_class_name("quantumWizTextinputPaperinputInput")[1] \
-                .send_keys(bot.student.last_name)
+            self.browser.find_elements_by_class_name("quantumWizTextinputPaperinputInput")[1]\
+                        .send_keys(bot.student.last_name)
 
-            self.browser.find_elements_by_class_name("quantumWizTextinputPaperinputInput")[2] \
-                .send_keys(str(bot.student.id))
-        except:
-            self.browser.quit()
+            self.browser.find_elements_by_class_name("quantumWizTextinputPaperinputInput")[2]\
+                        .send_keys(str(bot.student.id))
+        except Exception as error:
+            warn(
+                error,
+                "Could not load School Attendance page",
+                "Proceeding without submitting attendance"
+            )
+            return
 
         try:
             choices = self.browser.find_elements_by_class_name("freebirdFormviewerViewNumberedItemContainer")[3]\
@@ -119,16 +208,21 @@ class web_bot:
                     choices[i].click()
                     break
                 i += 1
-        except:
-            log("Error: Counselor not found in Attendance Form.\nProceeding without Attendance")
+        except Exception as warning:
+            warn(
+                warning,
+                "Counselor not found in Attendance Form."
+                "Proceeding without Attendance"
+            )
             return
 
         # self.browser.find_element_by_class_name("freebirdFormviewerViewNavigationSubmitButton").send_keys(Keys.RETURN)
 
         log("Submitted School Attendance")
 
-    def go_to_class(self, class_number):
+    def join_Google_Meet(self, class_number):
         self.browser.get(self.student.schedule[class_number]["meet url"])
+        log(f"Joining '{self.student.schedule[class_number]['course name']}'...")
 
         try:
             WebDriverWait(self.browser, 5).until(
@@ -137,12 +231,17 @@ class web_bot:
                 )
             ).click()
             self.browser.find_elements_by_tag_name("svg")[6].click()
-            # TODO: Click "Join now" button
-        except:
-            self.browser.quit()
 
-        log(f"Entered ~~{self.student.schedule[class_number]['name']}~~")
-        time.sleep(0.8)
+            # TODO: Click "Join now" button
+        except Exception as warning:
+            self.browser.get("https://google.com/")
+            warn(
+                warning,
+                "Could not load {} meet link",
+                "Make sure the link is correct in \"student.json\""
+            )
+
+        log(f"Entered ~~{self.student.schedule[class_number]['course name']}~~.")
 
 
 if __name__ == "__main__":
@@ -150,9 +249,10 @@ if __name__ == "__main__":
 
     bot.google_logIn()
     # bot.submit_school_attendance()
-    # bot.go_to_class(0)
+    bot.join_Google_Meet(0)
 
     # -for when no mic/cam perms: bot.browser.find_elements_by_class_name("U26fgb")[4].send_keys(Keys.RETURN)
 
     input("\nPress ENTER to Quit")
     bot.browser.quit()
+    exit(0)
